16052705ad81b45caf7eabf57f58de4f2eb454fc
Bruno <Brutalantunesfig16@gmail.com>
2025-07-01 23:05:55 -0300

    JS GAME tudo maisculo

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -145,5 +144,3 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
-
-
